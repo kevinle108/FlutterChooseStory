69e887c6f59bc8753b9c25dda07b98470f59384f
Kevin Le <kevinle108@gmail.com>
2021-06-10 08:20:38 -0400

    refactored nextStory

--- lib/story.dart
+++ lib/story.dart
@@ -2,8 +2,10 @@ class Story {
   String storyTitle;
   String choice1;
   String choice2;
+  int target1;
+  int target2;
 
-  Story({this.storyTitle, this.choice1, this.choice2});
+  Story({this.storyTitle, this.choice1, this.choice2, this.target1, this.target2});
 
 }
 
--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -3,34 +3,52 @@ import 'story.dart';
 class StoryBrain {
   List<Story> _storyData = [
     Story(
-        storyTitle:
-        'Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: "Need a ride, boy?".',
-        choice1: 'I\'ll hop in. Thanks for the help!',
-        choice2: 'Better ask him if he\'s a murderer first.'),
+      storyTitle:
+          'Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: "Need a ride, boy?".',
+      choice1: 'I\'ll hop in. Thanks for the help!',
+      choice2: 'Better ask him if he\'s a murderer first.',
+      target1: 2,
+      target2: 1,
+    ),
     Story(
-        storyTitle: 'He nods slowly, unphased by the question.',
-        choice1: 'At least he\'s honest. I\'ll climb in.',
-        choice2: 'Wait, I know how to change a tire.'),
+      storyTitle: 'He nods slowly, unphased by the question.',
+      choice1: 'At least he\'s honest. I\'ll climb in.',
+      choice2: 'Wait, I know how to change a tire.',
+      target1: 2,
+      target2: 3,
+    ),
     Story(
-        storyTitle:
-        'As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glovebox. Inside you find a bloody knife, two severed fingers, and a cassette tape of Elton John. He reaches for the glove box.',
-        choice1: 'I love Elton John! Hand him the cassette tape.',
-        choice2: 'It\'s him or me! You take the knife and stab him.'),
+      storyTitle:
+          'As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glovebox. Inside you find a bloody knife, two severed fingers, and a cassette tape of Elton John. He reaches for the glove box.',
+      choice1: 'I love Elton John! Hand him the cassette tape.',
+      choice2: 'It\'s him or me! You take the knife and stab him.',
+      target1: 5,
+      target2: 4,
+    ),
     Story(
-        storyTitle:
-        'What? Such a cop out! Did you know traffic accidents are the second leading cause of accidental death for most adult age groups?',
-        choice1: 'Restart',
-        choice2: ''),
+      storyTitle:
+          'What? Such a cop out! Did you know traffic accidents are the second leading cause of accidental death for most adult age groups?',
+      choice1: 'Restart',
+      choice2: '',
+      target1: -1,
+      target2: -1,
+    ),
     Story(
-        storyTitle:
-        'As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in.',
-        choice1: 'Restart',
-        choice2: ''),
+      storyTitle:
+          'As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in.',
+      choice1: 'Restart',
+      choice2: '',
+      target1: -1,
+      target2: -1,
+    ),
     Story(
-        storyTitle:
-        'You bond with the murderer while crooning verses of "Can you feel the love tonight". He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: "Try the pier".',
-        choice1: 'Restart',
-        choice2: '')
+      storyTitle:
+          'You bond with the murderer while crooning verses of "Can you feel the love tonight". He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: "Try the pier".',
+      choice1: 'Restart',
+      choice2: '',
+      target1: -1,
+      target2: -1,
+    )
   ];
 
   int _storyNumber = 0;
@@ -44,21 +62,31 @@ class StoryBrain {
   void restart() => _storyNumber = 0;
 
   void nextStory(int choiceNumber) {
-    if (_storyNumber == 0 && choiceNumber == 1) _storyNumber = 2;
-    else if (_storyNumber == 0 && choiceNumber == 2) _storyNumber = 1;
-    else if (_storyNumber == 1 && choiceNumber == 1) _storyNumber = 2;
-    else if (_storyNumber == 1 && choiceNumber == 2) _storyNumber = 3;
-    else if (_storyNumber == 2 && choiceNumber == 1) _storyNumber = 5;
-    else if (_storyNumber == 2 && choiceNumber == 2) _storyNumber = 4;
-    else {
-      // finished
+    if (_storyData[_storyNumber].target1 == -1 && _storyData[_storyNumber].target2 == -1) {
       restart();
     }
+    else {
+      _storyNumber = (choiceNumber == 1) ? _storyData[_storyNumber].target1 : _storyData[_storyNumber].target2;
+    }
+    // if (_storyNumber == 0 && choiceNumber == 1)
+    //   _storyNumber = 2;
+    // else if (_storyNumber == 0 && choiceNumber == 2)
+    //   _storyNumber = 1;
+    // else if (_storyNumber == 1 && choiceNumber == 1)
+    //   _storyNumber = 2;
+    // else if (_storyNumber == 1 && choiceNumber == 2)
+    //   _storyNumber = 3;
+    // else if (_storyNumber == 2 && choiceNumber == 1)
+    //   _storyNumber = 5;
+    // else if (_storyNumber == 2 && choiceNumber == 2)
+    //   _storyNumber = 4;
+    // else {
+    //   // finished
+    //   restart();
+    // }
   }
 
   bool buttonShouldBeVisible() {
-    return _storyNumber == 0 || _storyNumber == 1 ||_storyNumber == 2;
+    return _storyNumber == 0 || _storyNumber == 1 || _storyNumber == 2;
   }
-
 }
-
